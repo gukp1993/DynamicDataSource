33de1321d3cd8a86dcc4381b078c08883735f64b
gukp1993 <gkptorres@126.com>
2019-01-01 17:09:17 +0800

    2019/1/1 新年要努力喔

--- src/main/java/cn/cry/DynamicDataSource/dao/ServiceDao.java
+++ src/main/java/cn/cry/DynamicDataSource/dao/ServiceDao.java
@@ -3,6 +3,8 @@ package cn.cry.DynamicDataSource.dao;
 import cn.cry.DynamicDataSource.domain.ServiceRequest;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 /**
  * @author gukepeng
  * @since 2018/12/4
@@ -12,7 +14,11 @@ public interface ServiceDao {
 
     String getServiceNameByFwsPinAndId(String fwsPin, Long id);
 
-    String getServiceNameBy3Params(@Param("a")String fwsPin, @Param("b") Long id, @Param("c") String serviceCode);
+    String getServiceNameBy3Params(@Param("a") String fwsPin, @Param("b") Long id, @Param("c") String serviceCode);
 
     String getServiceNameByRequest(@Param("request") ServiceRequest request);
+
+    List<String> listServiceNameByFwsPins(List<String> fwsPinList);
+
+    List<String> listServiceNameByRequest(List<ServiceRequest> requestList);
 }
--- src/main/java/cn/cry/DynamicDataSource/junit/MybatisTest.java
+++ src/main/java/cn/cry/DynamicDataSource/junit/MybatisTest.java
@@ -6,6 +6,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 
 /**
  * 1.单个入参（非java对象）：java接口中不使用@Param注解，xml中随意命名；接口中使用@Param注解，xml中必须使用@Param注解定义的名字或者param1
@@ -36,6 +37,7 @@ public class MybatisTest extends JunitTest {
         System.out.println(serviceDao.getServiceNameBy3Params("gkp",123L,"FW_GOODS-123"));
     }
 
+    @Ignore
     @Test
     public void getServiceNameByJavaBean(){
         ServiceRequest request = new ServiceRequest();
@@ -43,5 +45,18 @@ public class MybatisTest extends JunitTest {
         request.setId(123L);
         request.setServiceCode("FW_GOODS-123");
         System.out.println(serviceDao.getServiceNameByRequest(request));
+    }
+
+    @Test
+    public void listServiceNameByFwsPins(){
+        System.out.println(serviceDao.listServiceNameByFwsPins(Arrays.asList("gkp","gkp")));
+    }
+
+    @Ignore
+    @Test
+    public void listServiceNameByJavaBean(){
+        ServiceRequest request = new ServiceRequest();
+        request.setFwsPin("gkp");
+        System.out.println(serviceDao.listServiceNameByRequest(Arrays.asList(request,request)));
     }
 }
